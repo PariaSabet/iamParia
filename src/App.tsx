ddfad63abbbc1a3e482591ea12a72da6cbe7445c
Paria Sabet <paria20@gmail.com>
2024-11-12 23:32:40 -0800

    windows xp loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,46 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 5000) // 10 seconds
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <>
-      <div className="windows__bg">
-        <div className="windows__bg--inner">
-          <div className="windows__logo">
-            <div className="windows__logo--inner red"></div>
-            <div className="windows__logo--inner green"></div>
-            <div className="windows__logo--inner blue"></div>
-            <div className="windows__logo--inner yellow"></div>
-          </div>
-          <div className="windows__name">
-            <p>Microsoft</p>
-            <div className="windows__name--inner">
-              Windows<span>xp</span>
+      {isLoading ? (
+        <div className="windows__bg">
+          <div className="windows__bg--inner">
+            <div className="windows__logo">
+              <div className="windows__logo--inner red"></div>
+              <div className="windows__logo--inner green"></div>
+              <div className="windows__logo--inner blue"></div>
+              <div className="windows__logo--inner yellow"></div>
+            </div>
+            <div className="windows__name">
+              <p>Microsoft</p>
+              <div className="windows__name--inner">
+                Windows<span>xp</span>
+              </div>
+            </div>
+            <div className="windows__bg--loading">
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
             </div>
           </div>
-          <div className="windows__bg--loading">
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </div>
         </div>
-      </div>
+      ) : (
+        <div className="welcome-screen">Welcome to Paria's PC</div>
+      )}
     </>
   )
 }
