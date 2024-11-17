60215969d0f7a823eb58b81b5377346c8d554be8
Paria Sabet <paria20@gmail.com>
2024-11-16 23:57:06 -0800

    initiated the start menu

--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -1,5 +1,9 @@
 import './StartMenu.css'
-
+import emailIcon from '../assets/icons/Email.png'
+import musicIcon from '../assets/icons/music.png'
+import videoIcon from '../assets/icons/videos.png'
+import pictureIcon from '../assets/icons/pictures.png'
+import computerIcon from '../assets/icons/computer.png'
 interface StartMenuProps {
   isOpen: boolean
   onClose: () => void
@@ -11,48 +15,88 @@ export function StartMenu({ isOpen, onClose, className = '' }: StartMenuProps) {
 
   return (
     <div
-      className={`fixed bottom-10 left-0 bg-[#000080] text-white 
+      className={`fixed bottom-10 left-0
         min-h-96 shadow-lg
-        w-full md:w-[320px] md:left-0 
+        w-full md:w-[320px] md:left-0 text-gray-950
         ${className}`}
     >
       <div className="start-menu-overlay" onClick={onClose} />
       <div className="start-menu">
-        <div className="start-menu-left">
-          <div className="menu-item">
+        <div className="start-menu-left ">
+          {/* <div className="menu-item">
             <img src="/icons/ie.png" alt="Internet" />
             <span>Internet Explorer</span>
-          </div>
-          <div className="menu-item">
-            <img src="/icons/email.png" alt="Email" />
-            <span>E-mail</span>
-          </div>
-          <div className="menu-item">
+          </div> */}
+          <a
+            href="mailto:pariasabet13@gmail.com?subject=Hello from your portfolio&body=Hi, I found your website and..."
+            className="menu-item"
+          >
+            <div className="flex items-center">
+              <img src={emailIcon} alt="Email" />
+              <span>E-mail me</span>
+            </div>
+          </a>
+          {/* <div className="menu-item">
             <img src="/icons/msn.png" alt="MSN" />
             <span>MSN Messenger Service</span>
-          </div>
+          </div> */}
         </div>
         <div className="start-menu-right">
-          <div className="menu-item">
+          {/* <div className="menu-item">
             <img src="/icons/documents.png" alt="Documents" />
             <span>My Documents</span>
-          </div>
-          <div className="menu-item">
-            <img src="/icons/pictures.png" alt="Pictures" />
-            <span>My Pictures</span>
-          </div>
-          <div className="menu-item">
-            <img src="/icons/music.png" alt="Music" />
-            <span>My Music</span>
-          </div>
-          <div className="menu-item">
-            <img src="/icons/computer.png" alt="Computer" />
-            <span>My Computer</span>
-          </div>
-          <div className="menu-item">
+          </div> */}
+          <a
+            href="https://instagram.com/pariasabet13"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="menu-item"
+          >
+            <div className="flex items-center">
+              <img src={pictureIcon} alt="Pictures" />
+              <span>My Pictures</span>
+            </div>
+          </a>
+
+          <a
+            href="https://www.youtube.com/@pariasabet13"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="menu-item"
+          >
+            <div className="flex items-center">
+              <img src={videoIcon} alt="Videos" />
+              <span>My Videos</span>
+            </div>
+          </a>
+
+          <a
+            href="https://open.spotify.com/user/paria_n_s?si=8903d2fec10f4c5b"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="menu-item"
+          >
+            <div className="flex items-center">
+              <img src={musicIcon} alt="Music" />
+              <span>My Music</span>
+            </div>
+          </a>
+
+          <a
+            href="https://apple.fandom.com/wiki/Apple_M2_Max"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="menu-item"
+          >
+            <div className="flex items-center">
+              <img src={computerIcon} alt="Computer" />
+              <span>My Computer</span>
+            </div>
+          </a>
+          {/* <div className="menu-item">
             <img src="/icons/control-panel.png" alt="Control Panel" />
             <span>Control Panel</span>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
